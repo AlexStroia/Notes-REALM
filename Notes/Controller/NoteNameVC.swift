d4276fe22fd2ab7ff677ad342c8c3cd46713ded7
Stroia Alexandru <stroia.alexandru@assist.ro>
2018-06-08 15:52:18 +0300

    Added implementation of Realm for first screen - DONE

--- Notes/Controller/NoteNameVC.swift
+++ Notes/Controller/NoteNameVC.swift
@@ -7,68 +7,83 @@
 //
 
 import UIKit
-import CoreData
+import RealmSwift
+
 class NoteNameVC: UIViewController {
     
-    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    //MARK - Initialize REALM
+    let realm = try! Realm()
+    
     
     @IBOutlet weak var textField: UITextField!
-    var notesArray = [Note]()
+    var notesArray: Results<Note>!
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadFromCoreData()
+        loadFromRealm()
     }
     
     @IBAction func Tap(_ sender: UIButton) {
         if (textField.text?.trimmingCharacters(in: .whitespaces).isEmpty)! {
             textField.placeholder = "Textfield is empty"
         } else {
-//            let note = Note(context: context)
-//            note.name = textField.text
-//            note.registredDate = Date()
-//            notesArray.append(note)
-            saveToCoreData()
+            let note = Note()
+            note.name = textField.text!
+            note.registredDate =  "\(Date())"
+            saveToRealm(note: note)
             textField.resignFirstResponder()
         }
-         updateUIAfterEdit()
+        updateUIAfterEdit()
     }
-    
     
     private func updateUIAfterEdit() {
         textField.text = ""
         view.endEditing(true)
     }
     
-
     private func update() {
         if let indexPath = tableView.indexPathForSelectedRow {
-        notesArray[indexPath.row].setValue("New Name", forKey: "name")
+            notesArray[indexPath.row].setValue("New Name", forKey: "name")
         }
     }
     
-    private func saveToCoreData() {
+    private func saveToRealm(note: Note? = nil) {
         do {
-            try context.save()
-            print("Saved user with succes")
+            try realm.write {
+                realm.add(note!)
+                print("Added with succes!")
+            }
         } catch {
-            print("Error has thrown: \(error)")
+            print("\(error)")
+        }
+        
+        tableView.reloadData()
+    }
+    
+    private func deleteFromRealm(note: Note) {
+        do {
+            try realm.write {
+                realm.delete(note)
+                print("Deleted with succes!")
+            }
+        } catch {
+            print("\(error)")
         }
         tableView.reloadData()
     }
     
-    private func loadFromCoreData() {
-        
+    //MARK - Fetch the data of type Note from the database
+    private func loadFromRealm() {
+        notesArray = realm.objects(Note.self)
+        tableView.reloadData()
     }
-    
     
     @IBAction func deleteBtnAction(_ sender: UIButton) {
         let buttonPressedPosition = sender.convert(CGPoint.zero, to: self.tableView)
         let indexPath = self.tableView.indexPathForRow(at: buttonPressedPosition)
-//        context.delete(notesArray[indexPath!.row])
-        notesArray.remove(at: indexPath!.row)
-        saveToCoreData()
+        let objItem = notesArray[indexPath!.row]
+        deleteFromRealm(note: objItem)
     }
 }
 
@@ -79,7 +94,7 @@ extension NoteNameVC: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-//        cell.textLabel?.text = notesArray[indexPath.row].name ?? "Invalid name"
+        cell.textLabel?.text = notesArray[indexPath.row].name ?? "Invalid name"
         return cell
     }
     
